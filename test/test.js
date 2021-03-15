8f420f848dd99e6d2c603ec9780db93a4cac0b35
miiwu <i.miiwu@outlook.com>
2021-03-15 14:24:41 +0800

    feat: add typeof_nullish(), typeof_prototype(), typeof_source()

--- test/test.js
+++ test/test.js
@@ -1,5 +1,31 @@
-import { string } from "../src/utility/x.utility.js";
+import { typeof_source, typeof_nullish, typeof_prototype } from "../src/utility/typeof.utility.js";
 
 export let source = (function () {
-    console.log(string);
+    typeof_source(null, {
+        nullish: (type) => console.log(type, `<- typeof_source of null, from callback.nullish`),
+        prototype: ({ type }) => console.log(type, `<- typeof_source of null, from callback.prototype`),
+    });
+    typeof_source([], {
+        nullish: (type) => console.log(type, `<- typeof_source of [], from callback.nullish`),
+        prototype: ({ type }) => console.log(type, `<- typeof_source of [], from callback.prototype`),
+    });
+
+    console.log(`------------------------------------`);
+
+    // console.log(typeof_prototype(null)); // source have no prototype, may be undefine or null
+    console.log(typeof_prototype(""));
+    console.log(typeof_prototype([]));
+
+    typeof_prototype({}, ({ type }) => {
+        console.log(type, `<- typeof_prototype of {}`);
+    });
+
+    console.log(`------------------------------------`);
+
+    console.log(typeof_nullish(null));
+    console.log(typeof_nullish(undefined));
+
+    typeof_nullish("undefined", (type) => {
+        console.log(type, `<- typeof_nullish of "undefined"`);
+    });
 })();
